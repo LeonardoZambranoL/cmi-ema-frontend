112f80f8b4075e0f76286456548234f788ec0b50
LZ <ge94zeg@mytum.de>
2024-01-28 23:01:25 +0100

    image on signin

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -33,7 +33,7 @@ export default function Page() {
     setEmailSent(true);
   };
   return (
-    <div className="ml-2 mr-2 md:grid md:grid-cols-12">
+    <div className="ml-3 mr-3 md:grid md:grid-cols-12">
       <div className="md:col-span-2 lg:col-span-4" />
       <div className="sm:col-span-10 md:col-span-8 lg:col-span-4 my-40">
         <div className={!emailSent ? "hidden" : "container"}>
@@ -44,55 +44,61 @@ export default function Page() {
             <BigHeader> Revisa tu correo!</BigHeader>
           </div>
         </div>
-        <div>
-          <Card className={emailSent ? "hidden" : "block"}>
-            <CardHeader>
-              <CardTitle>
-                <MediumHeader>Ya pues registrate.</MediumHeader>
-              </CardTitle>
-              <CardDescription>
-                <span className="w-full flex flex-row justify-center">
-                  <SmallHeader>Cual es pues cual es?</SmallHeader>
-                </span>
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-col gap-10">
-              <Input
-                type="email"
-                placeholder="ema@ema.com"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </CardContent>
-            <CardFooter className="w-full flex flex-row justify-center">
-              <AlertDialog>
-                <AlertDialogTrigger
-                  onClick={handleSubmit}
-                  className="bg-background hover:bg-secondary border-solid border-2 border-primary text-primary font-semibold py-3 rounded-lg mt-2 w-full md:w-3/5"
-                >
-                  DALEE
-                </AlertDialogTrigger>
-                <AlertDialogContent>
-                  <AlertDialogHeader>
-                    <AlertDialogTitle>
-                      <SmallHeader>Te hemos enviado un correo.</SmallHeader>
-                    </AlertDialogTitle>
-                    <AlertDialogDescription>
-                      Instrucciones sobre como completar tu proceso de
-                      inscripción han sido enviadas a&nbsp;
-                      <u>
-                        <b>{email}</b>
-                      </u>
-                      .
-                    </AlertDialogDescription>
-                  </AlertDialogHeader>
-                  <AlertDialogFooter>
-                    <AlertDialogAction>OK</AlertDialogAction>
-                  </AlertDialogFooter>
-                </AlertDialogContent>
-              </AlertDialog>
-            </CardFooter>
-          </Card>
-        </div>
+        <Card className={`rounded-none ${emailSent ? "hidden" : "block"}`}>
+          <div className="grid grid-cols-12">
+            <div className="bg-[url('/orange_bg.png')] col-span-2 md:col-span-3"></div>
+            <div className="col-span-10 md:col-span-9">
+              <CardHeader>
+                <CardTitle>
+                  <MediumHeader>Registrate</MediumHeader>
+                </CardTitle>
+                <CardDescription>
+                  <span className="w-full flex flex-row justify-center">
+                    <SmallHeader>A la CMI 2024</SmallHeader>
+                  </span>
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="flex flex-col gap-10">
+                <div className="flex flex-row justify-center">
+                  <Input
+                    type="email"
+                    placeholder="ema@ema.com"
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-4/5 md:w-3/5"
+                  />
+                </div>
+              </CardContent>
+              <CardFooter className="w-full flex flex-row justify-center">
+                <AlertDialog>
+                  <AlertDialogTrigger
+                    onClick={handleSubmit}
+                    className="bg-background hover:bg-secondary border-solid border-2 border-primary text-primary font-semibold py-3 rounded-lg mt-2 w-4/5 md:w-3/5"
+                  >
+                    DALEE
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>
+                        <SmallHeader>Te hemos enviado un correo.</SmallHeader>
+                      </AlertDialogTitle>
+                      <AlertDialogDescription>
+                        Instrucciones sobre como completar tu proceso de
+                        inscripción han sido enviadas a&nbsp;
+                        <u>
+                          <b>{email}</b>
+                        </u>
+                        .
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogAction>OK</AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              </CardFooter>
+            </div>
+          </div>
+        </Card>
       </div>
     </div>
   );
