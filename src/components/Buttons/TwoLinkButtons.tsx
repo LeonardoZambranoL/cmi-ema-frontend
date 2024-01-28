ae76726c84c853b5cd2f140b8738c5f75370d91c
LZ <ge94zeg@mytum.de>
2024-01-28 16:07:29 +0100

    minumal widths added

--- src/components/Buttons/TwoLinkButtons.tsx
+++ src/components/Buttons/TwoLinkButtons.tsx
@@ -17,13 +17,13 @@ export default function TwoLinkButtons({
   return (
     <div className="mt-2 flex flex-row gap-4 max-w-full">
       <Link href={callToActionLink} className="w-2/3">
-        <Button className="bg-background hover:bg-secondary border-solid border-2 border-primary text-primary font-semibold py-6 w-full">
-          <span className="truncate">{callToActionContent}</span>
+        <Button className="bg-background hover:bg-secondary border-solid border-2 border-primary text-primary font-semibold py-6 w-full min-w-32">
+          <span className="">{callToActionContent}</span>
         </Button>
       </Link>
       <Link href={otherLink} className="w-1/3">
-        <Button className="bg-primary text-secondary font-semibold py-6 w-full truncate">
-          <span className="truncate">{otherContent}</span>
+        <Button className="bg-primary hover:bg-primary/70 text-secondary font-semibold py-6 w-full min-w-24">
+          <span className="p-5">{otherContent}</span>
         </Button>
       </Link>
     </div>
