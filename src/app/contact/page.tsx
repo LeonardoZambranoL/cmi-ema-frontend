37029d0216e9b6f7d6da89cc89d7848e96ecc5d1
LeonardoZambranoL <zambranoleonardo0812@gmail.com>
2024-02-19 00:41:10 +0100

    contact page done

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,27 @@
-import BigHeader from "@/components/Headers/BigHeader";
-import MutedParagraph from "@/components/Paragraph/MutedParagraph";
+import { getContactEmail, getContactPhone } from "@/lib/contact/utils";
+import MediumHeader from "@/components/Headers/MediumHeader";
+import StandardParagraph from "@/components/Paragraph/StandardParagraph";
+import MailTo from "@/components/Contact/MailTo";
 
 export default function Page() {
+  const contactPhone = getContactPhone();
   return (
-    <>
-      <BigHeader>Hey, I just met you, and this is crazy</BigHeader>
-      <div className="mt-5">
-        <MutedParagraph> But here&#39;s my number (+49 176 ********), so <b><u>call me</u></b>, maybe. </MutedParagraph>
+    <div className="container mb-20">
+      <div className="container mx-auto max-w-screen-lg px-4 py-8">
+        <div className="text-center">
+          <MediumHeader>Información de contacto</MediumHeader>
+        </div>
+
+        <div className="my-10 flex flex-col items-center">
+          <StandardParagraph>
+            Si tienes alguna duda, pregunta, recomendación o comentario
+            relacionado con la olimpiada o temas similares, no dudes en
+            escribir un correo electrónico a <MailTo subject="" body="" /> o
+            contactarnos al número&nbsp;
+            <span className="font-semibold">{contactPhone}</span>.
+          </StandardParagraph>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
