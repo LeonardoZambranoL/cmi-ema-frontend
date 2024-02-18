dd08e84eca2b680e2978285bfbc2f56332468cfc
LeonardoZambranoL <zambranoleonardo0812@gmail.com>
2024-02-18 19:51:43 +0100

    better ratios

--- src/components/Headers/BigHeader.tsx
+++ src/components/Headers/BigHeader.tsx
@@ -6,6 +6,6 @@ type Props = {
 
 export default function BigHeader({ children }: Props) {
   return (
-    <h1 className="font-bold text-2xl md:text-5xl lg:text-7xl">{children}</h1>
+    <h1 className="font-bold text-3xl md:text-5xl lg:text-7xl">{children}</h1>
   );
 }
--- src/components/Headers/MediumHeader.tsx
+++ src/components/Headers/MediumHeader.tsx
@@ -6,6 +6,6 @@ type Props = {
 
 export default function MediumHeader({ children }: Props) {
   return (
-    <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl">{children}</h1>
+    <h1 className="font-bold text-2xl md:text-4xl lg:text-5xl">{children}</h1>
   );
 }
